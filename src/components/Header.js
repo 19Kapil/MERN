ab11012355e78e64e020c52a27a7e675ee3394a7
19Kapil <parazulikapil777@gmail.com>
2023-07-09 21:37:49 +0545

    MERN

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ class Header extends Component{
                   ><img src="./images/lec.png" alt=""
                 /></a>
               </div>
-              <div className="search-bar">
+              <div className="search-bar">  
                 <form>
                   <input type="text" name="search" placeholder="Search..." />
                   <button type="submit"><i className="la la-search"></i></button>
