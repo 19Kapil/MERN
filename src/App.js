c001fe958cefc196e7caba1f6acc30e3d6956bca
19Kapil <parazulikapil777@gmail.com>
2023-07-09 21:39:01 +0545

    MERN

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/login';
-//import Home from './components/home';
+//import Login from './components/login';
+import Home from './components/home';
 
 function App() {
-  return <Login/>;
- // return <Home/>;
+  //return <Login/>;
+  return <Home/>;
 }
 
 export default App;
